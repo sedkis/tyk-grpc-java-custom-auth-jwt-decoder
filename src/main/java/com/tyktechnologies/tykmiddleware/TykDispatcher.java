ff0fbbe1654410fac9d89b048c2324c5dbc53a53
Sedky <sedkyaboushamalah@gmail.com>
2020-04-16 11:04:26 -0400

    takes JWT, decodes, creates new and resends

--- src/main/java/com/tyktechnologies/tykmiddleware/TykDispatcher.java
+++ src/main/java/com/tyktechnologies/tykmiddleware/TykDispatcher.java
@@ -1,5 +1,8 @@
 package com.tyktechnologies.tykmiddleware;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import coprocess.DispatcherGrpc;
 import coprocess.CoprocessObject;
 import coprocess.CoprocessSessionState;
@@ -29,20 +32,24 @@ public class TykDispatcher extends DispatcherGrpc.DispatcherImplBase {
     }
 
     CoprocessObject.Object MyAuthHook(CoprocessObject.Object request) {
-        // String authHeader = request.getRequest().getHeadersOrDefault("Authorization", "");
-        // if(!authHeader.equals(FOOBAR)) {
-        //     CoprocessObject.Object.Builder builder = request.toBuilder();
-        //     CoprocessReturnOverrides.ReturnOverrides retOverrides = CoprocessReturnOverrides.ReturnOverrides.newBuilder()
-        //     .setResponseCode(403)
-        //     .setResponseError("Not authorized")
-        //     .build();
+        // print the JWT or THROW
+        final String initialJWT = request.getRequest().getHeadersOrThrow("Authorization");
+        System.out.println(initialJWT);
 
-        //     builder.getRequestBuilder().setReturnOverrides(retOverrides);
-        //     return builder.build();
-        // }
+        // Decode Existing JWT
+        Algorithm algorithmHS = Algorithm.HMAC256("tyk123");
+        DecodedJWT decodedJWT = JWT.decode(initialJWT);
 
+        // Create new JWT
+        String newJwt = JWT.create()
+                .withClaim("role", decodedJWT.getClaim("role").asString())  // Injects "role" from old JWT
+                .withClaim("my-new-claim", "value of new claim")                              // Injects new claim
+                .withSubject(decodedJWT.getSubject())
+                .withIssuedAt(decodedJWT.getIssuedAt())
+                .sign(algorithmHS);
+
+        // Set the Tyk Session
         final long expiryTime = (System.currentTimeMillis() / 1000) + 5;
-
         CoprocessSessionState.SessionState session = CoprocessSessionState.SessionState.newBuilder()
         .setRate(1000.0)
         .setPer(1.0)
@@ -50,7 +57,8 @@ public class TykDispatcher extends DispatcherGrpc.DispatcherImplBase {
         .build();
 
         CoprocessObject.Object.Builder builder = request.toBuilder();
-        builder.putMetadata("newvalue", new Date().toString());
+        builder.getRequestBuilder().putSetHeaders("Authorization", newJwt);
+        builder.putMetadata("token", newJwt);
         builder.setSession(session);
 
         return builder.build();
